fba00db316f2b279214704a58dd8d3f014807664
Emanuele Di Bella <e.dibella.ext@teamsystem.com>
2024-10-09 09:48:30 +0200

    first commit

---
+++ src/test/java/com/web_systems/WebSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
